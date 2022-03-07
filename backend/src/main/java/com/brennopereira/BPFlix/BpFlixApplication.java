8019267fcb15c0863b034a45a4986c1087189a3d
Brenno <brennom@gmail.com>
2022-03-07 14:10:48 -0500

    First Commit

---
+++ backend/src/main/java/com/brennopereira/BPFlix/BpFlixApplication.java
@@ -0,0 +1,13 @@
+package com.brennopereira.BPFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpFlixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brennopereira/BPFlix/BpFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brennopereira.BPFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
