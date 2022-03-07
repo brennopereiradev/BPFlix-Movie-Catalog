7ae967877f01df1a349a3bf499a6aeda3d001969
Brenno <brennom@gmail.com>
2022-03-07 16:02:09 -0500

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,42 @@
-function Form(){
-    return(
-        <h1>Form</h1>
+import "./styles.css";
+
+function Form() {
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
+    return (
+        <div className="bpflix-form-container">
+            <img className="bpflix-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="bpflix-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="bpflix-form">
+                    <div className="form-group bpflix-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group bpflix-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="bpflix-form-btn-container">
+                        <button type="submit" className="btn btn-primary bpflix-btn">Salvar</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary bpflix-btn mt-3">Cancelar</button>
+            </div >
+        </div >
     );
 }
 
