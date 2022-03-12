db3b68e1320d12e6392d0d16e1ac2f832530cec6
Brenno Matthias Pereira <brennopereiradev@gmail.com>
2022-03-11 20:57:01 -0500

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,16 @@
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies?size=128&page=0`)
+    .then(response => {
+        console.log(response.data);
+    });
+
     return (
         <>
             <div className="container">
